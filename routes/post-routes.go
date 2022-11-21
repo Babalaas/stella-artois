dbe1c2f5e5e4d2fff02c0d80d9e8a5740da0c9cc
jared-heidt <jared14heidt@gmail.com>
2022-11-20 21:42:32 -0500

    implemented full CRUD functionality for posts

--- routes/post-routes.go
+++ routes/post-routes.go
@@ -7,5 +7,9 @@ import (
 )
 
 func RegisterPostRoutes(router *mux.Router) {
-	router.HandleFunc("/api/posts/", controllers.CreatePost).Methods("POST")
+	router.HandleFunc("/api/posts", controllers.GetPosts).Methods("GET")
+	router.HandleFunc("/api/posts/{id}", controllers.GetProductById).Methods("GET")
+	router.HandleFunc("/api/posts", controllers.CreatePost).Methods("POST")
+	router.HandleFunc("/api/posts/{id}", controllers.UpdatePost).Methods("PUT")
+	router.HandleFunc("/api/posts/{id}", controllers.DeletePost).Methods("DELETE")
 }

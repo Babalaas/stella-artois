d63783498cff32c7cdcbbda5d08bbd43660e0d6a
jared-heidt <jared14heidt@gmail.com>
2022-11-27 23:13:06 -0500

    removed viper, this doesnt work locally, seeing if it works on railway

--- server.go
+++ server.go
@@ -6,17 +6,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/spf13/viper"
 )
 
+type Env struct {
+	Port             string
+	ConnectionString string
+}
+
+func initEnv() *Env {
+
+	NewEnv := Env{
+		Port:             os.Getenv("PORT"),
+		ConnectionString: os.Getenv("CONNECTION_STRING"),
+	}
+
+	return &NewEnv
+}
+
+func main() {
+	MyEnv := initEnv()
+	db.Connect(MyEnv.ConnectionString)
+	db.Migrate()
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	routes.RegisterPostRoutes(router)
+
+	log.Println(fmt.Sprintf("Starting Server on port %s", MyEnv.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", MyEnv.Port), router))
+}
+
+/*
+// Viper Code
 type EnvConfig struct {
 	Port             string `mapstructure:"PORT"`
 	ConnectionString string `mapstructure:"CONNECTION_STRING"`
 }
 
-// Viper Code
+
 var MyEnvConfigs *EnvConfig
 
 func InitEnvConfigs() {
@@ -54,3 +84,4 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", MyEnvConfigs.Port), router))
 
 }
+*/

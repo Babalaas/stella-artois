c8fc788ab824f2418b0b8ccab16977fddd3567bd
jared-heidt <jared14heidt@gmail.com>
2022-11-27 21:15:12 -0500

    moved the location of AutomaticEnv call

--- server.go
+++ server.go
@@ -11,19 +11,22 @@ import (
 	"github.com/spf13/viper"
 )
 
-type envConfigs struct {
+type EnvConfig struct {
 	Port             string `mapstructure:"PORT"`
 	ConnectionString string `mapstructure:"CONNECTION_STRING"`
 }
 
-var EnvConfigs *envConfigs
+// Viper Code
+var MyEnvConfigs *EnvConfig
 
 func InitEnvConfigs() {
-	EnvConfigs = loadEnvVariables()
+	MyEnvConfigs = loadEnvVariables()
 }
 
-func loadEnvVariables() (config *envConfigs) {
+func loadEnvVariables() (config *EnvConfig) {
 	viper.SetConfigFile(".env")
+
+	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatal("Error reading env file", err)
@@ -34,22 +37,20 @@ func loadEnvVariables() (config *envConfigs) {
 		log.Fatal(err)
 	}
 
-	viper.AutomaticEnv()
-
 	return
 }
 
 func main() {
 
 	InitEnvConfigs()
-	db.Connect(EnvConfigs.ConnectionString)
+	db.Connect(MyEnvConfigs.ConnectionString)
 	db.Migrate()
 
 	router := mux.NewRouter().StrictSlash(true)
 
 	routes.RegisterPostRoutes(router)
 
-	log.Println(fmt.Sprintf("Starting Server on port %s", EnvConfigs.Port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", EnvConfigs.Port), router))
+	log.Println(fmt.Sprintf("Starting Server on port %s", MyEnvConfigs.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", MyEnvConfigs.Port), router))
 
 }

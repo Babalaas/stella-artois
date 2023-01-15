10474118381e9e2193df4a6d6bb90014d2a137a2
jared-heidt <jared14heidt@gmail.com>
2023-01-15 00:19:35 -0500

    added comment

--- handler/post_handler.go
+++ handler/post_handler.go
@@ -8,17 +8,9 @@ import (
 	"github.com/google/uuid"
 )
 
-// GET /{id}
+// GetPostByID cleans the id parameter and calls the PostService to get a post by based on the parameter (GET /{id})
 func (handler *Handler) GetPostById(c *gin.Context) {
 	reqId := c.Param("id")
-
-	/*
-		if !exists {
-			log.Panicf("Unable to extract post from request context for unknown reason: %v\n", c)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Post record not found!"})
-			return
-		}
-	*/
 
 	uid := uuid.Must(uuid.Parse(reqId))
 	ctx := c.Request.Context()

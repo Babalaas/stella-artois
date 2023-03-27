d373aecb819ec747a4340aba3f35da2c06c5b04a
jared-heidt <jared14heidt@gmail.com>
2023-03-27 14:07:43 -0400

    created post_comment model

---
+++ model/post_comment.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// PostComment outlines the database entity for a comment on a post
+type PostComment struct {
+	ID            uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
+	UserProfileID uuid.UUID `json:"user_profile_id" gorm:"type:uuid;not null"`
+	PostID        uuid.UUID `json:"post_id" gorm:"type:uuid;not null"`
+	DateCreated   time.Time `json:"date_created" gorm:"type:timestamp with time zone;not null"`
+	Content       string    `json:"content" gorm:"type:timestamp with time zone;not null"`
+}
+
+// BeforeCreate is the hook called before a PostComment is inserted into the database
+// Used to initialize default values
+func (comment *PostComment) BeforeCreate(db *gorm.DB) {
+	comment.DateCreated = time.Now().Local()
+}
